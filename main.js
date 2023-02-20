4c90663d229f9a73c254a59e3ad595e534e44bcf
MeblIkea <salulespotos@gmail.com>
2023-02-20 21:51:34 +0100

    Short button removed

--- main.js
+++ main.js
@@ -21,4 +21,11 @@ function waitForElm(selector) {
 waitForElm("#contents").then( function () {
     document.getElementById("contents").remove();
     console.log("%c Youtube is now optimized!", 'background: #222; color: #badada; font-size: 20px; font-weight: bold;');
+    console.log("Videos removed");
+});
+
+waitForElm(".ytd-guide-section-renderer").then( function () {
+    for (let element of document.getElementsByTagName("a")) {
+        if (element.getAttribute("title") === "Shorts") {element.remove();console.log("Shorts button removed")}
+    }
 });
