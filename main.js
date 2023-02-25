219c8cf40847a34b694c337b0641ff4e69240b45
MeblIkea <salulespotos@gmail.com>
2023-02-25 21:31:51 +0100

    IT WILL WORK WOWOWOOSOFKSOJPFSDWBUJN WDGFIOKLJU?NH M%GSDNHKjgr m,j bhdxcfgrtm!kl;:dtfgbh klj,nm!; tfgbdhncm!l:okj,tgyhb klom!,nfftg k,hbljjmxfihklo,ùdgtnom iuk,nhjftgdxwh ,ujnsxftikwdmùgoimùjunhdsftw,hjoùn,^siwdk hdtfshtd896hdrt45778496h5dfgst84596shfgd84596shg84596hgf48569

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+function getStored(item, key) {
+    console.log(item)
+    console.log(item[key])
+    let value = item[key];
+    if (value === undefined) {
+        browser.storage.sync.set({[key]: true});
+        value = true
+    }
+    return [key, value];
+}
+
+browser.storage.sync.get("homepage", function (item) {
+    if (getStored(item, "homepage")[1] === true) {
+        waitForElm("#contents").then( function () {
+            document.getElementById("contents").remove();
+            console.log("Videos removed");
+        });
+    }
+})
+browser.storage.sync.get("shorts_button", function (item) {
+    if (getStored(item, "shorts_button")[1] === true) {
+    waitForElm(".ytd-guide-section-renderer").then( function () {
+    for (let element of document.getElementsByTagName("a")) {
+        if (element.getAttribute("title") === "Shorts") {element.remove();console.log("Shorts button removed")}
+    }});
+    }
+})
+
 function waitForElm(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -18,19 +46,12 @@ function waitForElm(selector) {
     });
 }
 
-waitForElm("#contents").then( function () {
-    document.getElementById("contents").remove();
-    console.log("%c Youtube is now optimized!", 'background: #222; color: #badada; font-size: 20px; font-weight: bold;');
-    console.log("Videos removed");
-});
-
-waitForElm(".ytd-guide-section-renderer").then( function () {
-    for (let element of document.getElementsByTagName("a")) {
-        if (element.getAttribute("title") === "Shorts") {element.remove();console.log("Shorts button removed")}
-    }
-});
-
-let NoYtbPort = browser.runtime.connect({name:"no-youtube"});
-NoYtbPort.onMessage.addListener((message) => {
-    console.log(message)
-})
+browser.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting === "hello")
+      sendResponse({farewell: "goodbye"});
+  }
+);
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
+browser.storage.sync.set({"homepage": false});
+
 document.getElementById("about").onclick = function () {
-    open("https://github.com/MeblIkea/NoYoutube")
+    open("https://github.com/MeblIkea/NoYoutube");
 }
 
 homepage_label = document.getElementById("dhomepage");
@@ -24,22 +26,12 @@ shorts_suggestions_label.getElementsByTagName("input")[0].onclick = function () 
         shorts_suggestions_label.getElementsByTagName("span")[0].innerText = "Disabled";
     }
 }
-shorts_button_label.getElementsByTagName("input")[0].onclick = function () {
-    if (shorts_button_label.getElementsByTagName("input")[0].checked) {
-        shorts_button_label.getElementsByTagName("span")[0].innerText = "Enabled";
-    } else {
-        shorts_button_label.getElementsByTagName("span")[0].innerText = "Disabled";
-    }
+
+function sendMessageToTabs() {
+    (async () => {
+        const [tab] = await browser.tabs.query({active: true, lastFocusedWindow: true});
+        await browser.tabs.sendMessage(tab.id, {greeting: "hello"});
+    })();
 }
 
-let NoYtbPort;
-
-function connected(p) {
-  NoYtbPort = p;
-  NoYtbPort.postMessage({greeting: "hi there content script!"});
-}
-
-browser.runtime.onConnect.addListener(connected);
-browser.browserAction.onClicked.addListener(() => {
-  NoYtbPort.postMessage({greeting: "they clicked the button!"});
-});
+sendMessageToTabs()
